5ea95d244103de57be49052a73f7a81bd2904914
OskaKordik <oskakordik14@gmail.com>
2015-08-20 18:30:40 +0300

    adds new methods

--- src/main/java/controller/Company.java
+++ src/main/java/controller/Company.java
@@ -1,6 +1,7 @@
 package controller;
 
 import model.Person;
+import model.Position;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,13 +14,72 @@ public class Company {
 
     public static void main(String[] args) {
         setRandomPerson();
-        //проверить, что в фирме есть хотя бы один директор, менеджер и бухгалтер
-        //или задать новых
 
-        //смоделировать работу сотрудников в течении месяца
-        //каждую неделю бухгалтер платит з/п
+        //проверка, что в фирме есть директор
+        if (!isContainsPosition(Position.Director)) {
+            Person person = new Person("Director");
+            person.setPositionList(Position.Director);
+            personList.add(person);
+        }
+        //проверка, что в фирме есть менеджер
+        if (!isContainsPosition(Position.Manager)) {
+            Person person = new Person("Manager");
+            person.setPositionList(Position.Manager);
+            personList.add(person);
+        }
+        //проверка, что в фирме есть бухгалтер
+        if (!isContainsPosition(Position.Accountant)) {
+            Person person = new Person("Accountant");
+            person.setPositionList(Position.Accountant);
+            personList.add(person);
+        }
+        try {
+            workMonth(); //запуск моделирования работы компании в течении месяца
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
-        //формирование суммарного отчета + сохранение в файл
+    /**
+     * Метод, запускающий работу сотрудников в течении дня
+     * @throws InterruptedException
+     */
+    private static void workDay() throws InterruptedException {
+        for (Person person : personList) {
+            Thread threadPerson = new Thread(person);
+            threadPerson.start();
+        }
+        Thread.sleep(8);
+    }
+
+    /**
+     * Метод, запускающий работу сотрудников в течении недели
+     * @throws InterruptedException
+     */
+    private static void workWeek() throws InterruptedException {
+        for (int i = 0; i < 5; i++) workDay();
+        //каждую неделю бухгалтер платит з/п - реализовать
+    }
+
+    /**
+     * Метод, запускающий работу сотрудников в течении месяца
+     * @throws InterruptedException
+     */
+    private static void workMonth() throws InterruptedException {
+        for (int i = 0; i < 4; i++) workWeek();
+        //формирование суммарного отчета + сохранение в файл - реализовать
+    }
+
+    /**
+     * Метод проверяет есть ли указанная должность в списке сотрудников
+     * @param position
+     * @return true or false
+     */
+    private static boolean isContainsPosition(Position position) {
+        for (Person person : personList) {
+            if (person.getPositionList().contains(position)) return true;
+        }
+        return false;
     }
 
     /**
@@ -28,9 +88,8 @@ public class Company {
     private static void setRandomPerson() {
         personList = new ArrayList<>();
         int countPersons = (int) (Math.random() * 90 + 10);
-        for (int i = 0; i < countPersons; i++) {
-            double workHoursPerDay = Math.random() * 7 + 1; //генерирует количество рабочих часов в день
-            personList.add(new Person("Сотрудник №" + i, workHoursPerDay)); //добавляет сотрудника в список
+        for (int i = 1; i <= countPersons; i++) {
+            personList.add(new Person("Сотрудник №" + i)); //добавляет сотрудника в список
         }
     }
 
