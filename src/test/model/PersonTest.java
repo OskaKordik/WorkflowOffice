878a394babb7b66e93e0ad391ecf4527862f2401
OskaKordik <oskakordik14@gmail.com>
2015-08-20 14:56:50 +0300

    add test for method getPositionList

---
+++ src/test/model/PersonTest.java
@@ -0,0 +1,21 @@
+package model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Тестирует работу класса Person
+ */
+public class PersonTest {
+
+    /**
+     * Тест проверяет корректное заполнение списка должностей при создании сотрудника
+     */
+    @Test
+    public void test_getPositionList() {
+        Person person = new Person("Vasya", 5);
+
+        assertTrue((person.getPositionList().size() > 0) && (person.getPositionList().size() <= 7));
+    }
+}
