9bbeb29e91b70e97200c5364a31cb12ebdebaab0
OskaKordik <oskakordik14@gmail.com>
2015-08-22 19:48:57 +0300

    removing unnecessary methods

--- src/main/java/controller/Company.java
+++ src/main/java/controller/Company.java
@@ -1,28 +1,13 @@
 package controller;
 
-import model.Position;
-
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * Класс Компания - контроллирует работу компании
  */
 public class Company {
 
-    private static Set<Position> necessaryPositions; //список обязательных должностей
+
 
     public static void main(String[] args) {
-
-        necessaryPositions = new HashSet<>(); //создаем список обязательных должностей
-        necessaryPositions.add(Position.Director);
-        necessaryPositions.add(Position.Manager);
-        necessaryPositions.add(Position.Accountant);
-        PersonController.INSTANCE.setNecessaryPositions(necessaryPositions);
-
-        PersonController.INSTANCE.runPersonController(); //создаем сотрудников
-
         WorkController.INSTANCE.runPersonController(); //запускаем работу
     }
-
 }
