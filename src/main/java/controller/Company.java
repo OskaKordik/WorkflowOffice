08f91d8df63dd595f228f7b1e2e001a040938a84
OskaKordik <oskakordik14@gmail.com>
2015-08-23 14:18:57 +0300

    dell spaces

--- src/main/java/controller/Company.java
+++ src/main/java/controller/Company.java
@@ -4,9 +4,6 @@ package controller;
  * Класс Компания - контроллирует работу компании
  */
 public class Company {
-
-
-
     public static void main(String[] args) {
         WorkController.INSTANCE.runPersonController(); //запускаем работу
     }
