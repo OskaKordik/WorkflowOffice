8ebc599c95adce7fda0f2de3a0b671d828237a72
OskaKordik <oskakordik14@gmail.com>
2015-08-24 13:50:29 +0300

    create test class for DirectorsController and test for method selectionOfDirectors

---
+++ src/test/controller/DirectorsControllerTest.java
@@ -0,0 +1,34 @@
+package controller;
+
+import model.Person;
+import model.Position;
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Тестирует работу контроллера директоров
+ */
+public class DirectorsControllerTest {
+
+    /**
+     * Тест проверяет корректность работы метода selectionOfDirectors()
+     */
+    @Test
+    public void testSelectionOfDirectors() {
+        Map<Person, Set<Position>> testList = new HashMap<>();
+        testList.put(PersonController.INSTANCE.createPerson("TestPerson"),
+                new HashSet<>(Arrays.asList(Position.Accountant, Position.Designer)));
+        testList.put(PersonController.INSTANCE.createPerson("TestPerson"),
+                new HashSet<>(Arrays.asList(Position.Manager, Position.Tester)));
+        testList.put(PersonController.INSTANCE.createPerson("Director"),
+                new HashSet<>(Arrays.asList(Position.Director)));
+        testList.put(PersonController.INSTANCE.createPerson("Director"),
+                new HashSet<>(Arrays.asList(Position.Director)));
+        List<Person> resultList = DirectorsController.INSTANCE.selectionOfDirectors(testList);
+
+        for (Person person : resultList) assertEquals("Director", person.getPersonName());
+    }
+}
