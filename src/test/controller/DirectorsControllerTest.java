0ff199e58b510a08e3f4792264009893d8c1c420
OskaKordik <oskakordik14@gmail.com>
2015-08-24 14:00:40 +0300

    added test testSelectionOfDirectorsAndSetAmountHoursOneInstructions

--- src/test/controller/DirectorsControllerTest.java
+++ src/test/controller/DirectorsControllerTest.java
@@ -31,4 +31,20 @@ public class DirectorsControllerTest {
 
         for (Person person : resultList) assertEquals("Director", person.getPersonName());
     }
+
+    /**
+     * Тест проверяет корректную установки времени выполнения одного задания для
+     * сотрудников с должностью директора
+     */
+    @Test
+    public void testSelectionOfDirectorsAndSetAmountHoursOneInstructions() {
+        Map<Person, Set<Position>> testList = new HashMap<>();
+        testList.put(PersonController.INSTANCE.createPerson("TestPerson"),
+                new HashSet<>(Arrays.asList(Position.Accountant, Position.Designer)));
+        testList.put(PersonController.INSTANCE.createPerson("Director"),
+                new HashSet<>(Arrays.asList(Position.Director)));
+        List<Person> resultList = DirectorsController.INSTANCE.selectionOfDirectors(testList);
+
+        for (Person person : resultList) assertEquals(1, person.getAmountHoursOneInstructions(), 0.0f);
+    }
 }
