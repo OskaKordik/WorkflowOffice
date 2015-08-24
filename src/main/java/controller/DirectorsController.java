25b3f6200da0135713b3e4953fe9a4f2360aa18e
OskaKordik <oskakordik14@gmail.com>
2015-08-24 13:36:59 +0300

    create class DirectorsController and added methods runDirectorsController, selectionOfDirectors

--- src/main/java/controller/DirectorsController.java
+++ src/main/java/controller/DirectorsController.java
@@ -1,8 +1,35 @@
 package controller;
 
+import model.Person;
+import model.Position;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Класс управляющий директорами
  */
 public class DirectorsController {
+    public static final DirectorsController INSTANCE = new DirectorsController();
+    private Map<Person, Set<Position>> personList; //список всех сотрудников
+    private List<Person> directorsList; //список директоров
 
+    public void runDirectorsController() {
+        personList = PersonController.INSTANCE.getPersonList(); //получаем список работников
+        directorsList = selectionOfDirectors(personList);
+    }
+
+    /**
+     * Метод выбирает из списка сотрудников только с должностью директора
+     * @param personList список всех сотрудников
+     * @return list список сотрудников с должностью директора
+     */
+    protected List<Person> selectionOfDirectors(Map<Person, Set<Position>> personList) {
+        List<Person> list = new ArrayList<>();
+        for (Map.Entry<Person, Set<Position>> person : personList.entrySet())
+            if (person.getValue().contains(Position.Director)) list.add(person.getKey());
+        return list;
+    }
 }
