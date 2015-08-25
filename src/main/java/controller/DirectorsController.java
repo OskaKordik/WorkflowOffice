9b00a21474607b8133490b5a1dd053cf5c3dba2d
OskaKordik <oskakordik14@gmail.com>
2015-08-25 15:08:18 +0300

    refactoring class DirectorsController

--- src/main/java/controller/DirectorsController.java
+++ src/main/java/controller/DirectorsController.java
@@ -14,7 +14,6 @@ public class DirectorsController {
     private static Map<Person, Set<Position>> personList; //список всех сотрудников
     private static List<Person> directorsList; //список директоров
     private static final Map<Position, String> taskList = new HashMap<>();
-    private static Map<Person, Set<Position>> personListOtherDirectors; //список всех сотрудников кроме директоров
 
     static { //заполнение списка распоряжений для сотрудников
         taskList.put(Position.Director, "раздать распоряжения сотрудникам");
@@ -27,17 +26,13 @@ public class DirectorsController {
 
     public void runDirectorsController() {
         personList = PersonController.INSTANCE.getPersonList(); //получаем список работников
-        personListOtherDirectors = selectionOfPersonsOtherDirectors(personList);
-        directorsList = selectionOfDirectors(personList);
+        directorsList = selectionOfDirectors(personList); //получаем список директоров
+        //распределение сотрудников между директорами
+        distributionOfPersons(directorsList, selectionOfPersonsOtherDirectors(personList));
+        //задание всем сотрудникам списка распоряжений
+        setDirectorsTaskList(directorsList, taskList);
 
-        for (Person director : directorsList) {
-            Director personDirector = (Director) director.getListPositions().get(Position.Director);
-            //передаем всем директорам список их сотрудников
-            personDirector.setPersonList(personListOtherDirectors);
-            //передаем список распоряжений
-            personDirector.setTaskList(taskList);
-        }
-
+        //workDirectors(directorsList); //запуск один раз - для проверки
         for (int i = 0; i < Company.MAX_WORKING_HOURS; i++) {
             workDirectors(directorsList);
             try {
@@ -46,6 +41,73 @@ public class DirectorsController {
                 e.printStackTrace();
             }
         }
+    }
+
+    /**
+     * Метод передает всем директорами список распоряжений
+     * @param directorsList список директоров
+     */
+    protected void setDirectorsTaskList(List<Person> directorsList, Map<Position, String> taskList) {
+        for (Person director : directorsList) {
+            Director personDirector = (Director) director.getListPositions().get(Position.Director);
+            //передаем список распоряжений
+            personDirector.setTaskList(taskList);
+        }
+    }
+
+    /**
+     * Метод распределяет всех сотрудников между директорами
+     * @param directorsList список директоров
+     * @param personListOtherDirectors список сотрудников кроме директоров
+     */
+    protected void distributionOfPersons(List<Person> directorsList, Map<Person, Set<Position>> personListOtherDirectors) {
+        int countListPersons = directorsList.size(); //кол-во списков
+        int countPersonsInList = personListOtherDirectors.size() / countListPersons; //кол-во сотрудников в списке
+        int countOtherPersonsInList = personListOtherDirectors.size() % countListPersons;
+
+        Iterator<Map.Entry<Person, Set<Position>>> iterator = personListOtherDirectors.entrySet().iterator();
+
+        //передаем всем директорам список их сотрудников
+        for (Person director : directorsList) {
+            Director personDirector = (Director) director.getListPositions().get(Position.Director);
+            Map<Person, Set<Position>> list = getSubListPersons(iterator, countPersonsInList);
+            if (list.size() > 0) personDirector.setPersonList(list);
+        }
+
+        //если остались еще не распределенные работники
+        if (countOtherPersonsInList > 0) {
+            for (Person director : directorsList) {
+                Director personDirector = (Director) director.getListPositions().get(Position.Director);
+                //передаем всем директорам список их сотрудников
+                Map<Person, Set<Position>> list = getSubListPersons(iterator, countPersonsInList);
+                if (list.size() > 0) {
+                    Map<Person, Set<Position>> tmpList = personDirector.getPersonList();
+                    //добавляем к существующему списку оставшихся сотрудников
+                    for (Map.Entry<Person, Set<Position>> person : list.entrySet()) {
+                        tmpList.put(person.getKey(), person.getValue());
+                        countOtherPersonsInList--; //нужно для подсчета - УБРАТЬ!
+                    }
+                    personDirector.setPersonList(tmpList);
+                }
+            }
+        }
+    }
+
+    /**
+     * Метод формирует подсписок сотрудников для директора
+     * @param iterator итератор списка всех сотрудников (кроме директоров)
+     * @param countPersonsInList кол-во сотрудников в списке
+     * @return подсписок сотрудников для директора
+     */
+    protected Map<Person, Set<Position>> getSubListPersons(Iterator<Map.Entry<Person, Set<Position>>> iterator, int countPersonsInList) {
+        Map<Person, Set<Position>> list = new HashMap<>();
+        for (int i = 0; i < countPersonsInList; i++) {
+            if (iterator.hasNext()) {
+                Map.Entry<Person, Set<Position>> thisEntry = iterator.next(); //переходим к следующему элементу списка
+                list.put(thisEntry.getKey(), thisEntry.getValue());
+            }
+        }
+        return list;
     }
 
     /**
@@ -91,7 +153,4 @@ public class DirectorsController {
         return list;
     }
 
-    public static Map<Position, String> getTaskList() {
-        return taskList;
-    }
 }
