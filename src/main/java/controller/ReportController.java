6132367a29aded3611126b75dc1a7c7a27641c13
OskaKordik <oskakordik14@gmail.com>
2015-08-26 18:53:39 +0300

    editing method runReportController - added write to file

--- src/main/java/controller/ReportController.java
+++ src/main/java/controller/ReportController.java
@@ -2,12 +2,19 @@ package controller;
 
 import model.Positions.Accountant;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Класс отвечающий за формирование отчета
  */
 public class ReportController {
     public static final ReportController INSTANCE = new ReportController();
     private static Accountant generalAccountant; //главный бухгалтер
+    private String fileName;
+    private List<String> dataFile;
 
     /**
      * Доступ к контроллеру осуществляется через INSTANCE
@@ -15,11 +22,22 @@ public class ReportController {
     private ReportController() {}
 
     public void runReportController() {
+        fileName = "report.txt";
+        dataFile = new ArrayList<>();
         generalAccountant = WorkController.INSTANCE.getGeneralAccountant();
-        System.out.println("Всего сотрудников : " + PersonController.INSTANCE.getPersonList().size());
-        System.out.println("Всего фрилансеров : " + PersonController.INSTANCE.getFreelancers().size());
-        System.out.println("-------------------------------------");
-        System.out.println("Всего выплачено сотрудникам : " + generalAccountant.getAllSalary());
-        System.out.println("Всего выплачено фрилансерам : " + generalAccountant.getAllSalaryFreelancers());
+        dataFile.add("Всего сотрудников : " + PersonController.INSTANCE.getPersonList().size());
+        dataFile.add("Всего фрилансеров : " + PersonController.INSTANCE.getFreelancers().size());
+        dataFile.add("-------------------------------------");
+        dataFile.add("Всего выплачено сотрудникам : " + generalAccountant.getAllSalary());
+        dataFile.add("Всего выплачено фрилансерам : " + generalAccountant.getAllSalaryFreelancers());
+
+        try (FileWriter fileWriter = new FileWriter(fileName, false)) {
+            for (String writeStr : dataFile) {
+                fileWriter.write(writeStr);
+                fileWriter.write(System.lineSeparator());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
